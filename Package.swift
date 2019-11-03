cc158be5b28447f2a49cde022c440d798132edd1
Bartek Zabicki <bartekzabicki@gmail.com>
2019-11-03 18:19:08 +0100

    Update version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,6 @@ let package = Package(
     name: "BZCalendar",
     platforms: [.iOS(.v11), .macOS(.v10_13)],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "BZCalendar",
             targets: ["BZCalendar"]),
@@ -16,8 +15,6 @@ let package = Package(
       .package(url: "https://github.com/bartekzabicki/Unicorns.git", from: "0.2.2"),
   ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "BZCalendar",
             dependencies: ["Unicorns"],
